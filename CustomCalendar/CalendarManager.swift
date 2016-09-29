45c89ff6856a3d7bfa825c77051b0d06e6fc0686
Ömer Faruk Öztürk <omer@omers-MacBook-Pro.local>
2016-09-29 12:34:51 +0300

    Initial Commit

---
+++ CustomCalendar/CalendarManager.swift
@@ -0,0 +1,9 @@
+//
+//  CalendarManager.swift
+//  CustomCalendar
+//
+//  Created by Ömer Faruk Öztürk on 29/09/2016.
+//  Copyright © 2016 omerfarukozturk. All rights reserved.
+//
+
+import Foundation
---
+++ CustomCalendar/Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  Extensions.swift
+//  CustomCalendar
+//
+//  Created by Ömer Faruk Öztürk on 29/09/2016.
+//  Copyright © 2016 omerfarukozturk. All rights reserved.
+//
+
+import Foundation
